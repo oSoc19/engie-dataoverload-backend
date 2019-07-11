ad824f50c44080dd6348acff1115d9002047095b
jodiDL <jodi@jodideloof.be>
2019-07-11 15:46:49 +0200

    calculate closest zip from database

--- queries/quiz.js
+++ queries/quiz.js
@@ -9,6 +9,24 @@ const pool = new Pool({
 
 const http_request = require('request');
 
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+    var dLon = deg2rad(lon2 - lon1);
+    var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2)
+        ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // Distance in km
+    return d;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI / 180)
+}
+
 const getSolarProdZip = (request, response) => {
     let zip = request.params.id;
 
@@ -18,13 +36,14 @@ const getSolarProdZip = (request, response) => {
         }
 
         let average = results.rows;
+        console.log(average);
 
-        if (average.avg) {
+        if (average[0].avg != null) {
             response.status(200).send(average);
         } else {
 
             var options = {
-                url: 'https://nominatim.openstreetmap.org/search?q=1790,Belgi%C3%AB&format=json',
+                url: 'https://nominatim.openstreetmap.org/search?q='+zip+',Belgi%C3%AB&format=json',
                 headers: {
                     'User-Agent': 'engie-data-overload'
                 }
@@ -35,11 +54,30 @@ const getSolarProdZip = (request, response) => {
                 let info = JSON.parse(body);
                 let lat = info[0].lat;
                 let long = info[0].lon;
-                console.log(lat + ", " + long);
-            });
 
-            pool.query("select avg(sp.value) from solar_prod_day sp join postal_codes p on p.customer_id = sp.customer_id where p.postal_code = $1;", [zip], (error, results) => {
-                response.status(200).send("average");
+                let closest_zip = 0;
+                let smallest_distance = Number.MAX_SAFE_INTEGER;
+                pool.query('select * from (select * from postal_codes where customer_id in (select customer_id from solar_prod_day)) a;', (error, results) => {
+
+                    results.rows.forEach(function (element) {
+                        let long1 = element.longitude;
+                        let lat1 = element.latitude;
+                        let distance = getDistanceFromLatLonInKm(lat1, long1,lat,long);
+                        if(distance < smallest_distance){
+                            smallest_distance = distance;
+                            closest_zip = element.postal_code;
+                        }
+                        console.log(smallest_distance);
+                    });
+
+                    console.log(closest_zip);
+                    
+                    
+                    pool.query("select avg(sp.value) from solar_prod_day sp join postal_codes p on p.customer_id = sp.customer_id where p.postal_code = $1;", [closest_zip], (error, results) => {
+                        let closest_average = results.rows;
+                        response.status(200).send(closest_average);
+                    });
+                });
             });
         }
 
