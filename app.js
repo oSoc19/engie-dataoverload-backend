5059ad537c26b7939339409425f1be1d35f87915
jodiDL <jodi@jodideloof.be>
2019-07-22 10:13:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -17,11 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(function(req, res, next) {
-
-  res.header("Access-Control-Allow-Origin", "*");
- 
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
- 
+  res.header("Access-Control-Allow-Origin", "*"); 
   next();
  });
 
